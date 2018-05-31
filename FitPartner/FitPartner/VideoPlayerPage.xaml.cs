35829b02175037499e675bebf2b268125f655371
Lőrincz Péter <peter.pal.lorincz@gmail.com>
2018-06-01 00:27:48 +0200

    polished mainpage + added videoplayerpage

--- FitPartner/FitPartner/VideoPlayerPage.xaml.cs
+++ FitPartner/FitPartner/VideoPlayerPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Octane.Xamarin.Forms.VideoPlayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,8 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Acr.UserDialogs;
+using Octane.Xamarin.Forms.VideoPlayer.Constants;
 
 namespace FitPartner
 {
@@ -15,6 +18,41 @@ namespace FitPartner
 		public VideoPlayerPage ()
 		{
 			InitializeComponent ();
-		}
-	}
+            index = 0;
+            Player.Source = VideoSource.FromResource(Queue[index]);
+
+
+            SubscribeEvents();
+            
+        }
+
+        private void SubscribeEvents()
+        {
+            Player.Failed += async (sender, e) => { await UserDialogs.Instance.AlertAsync("Video failed to load...", "Error", "OK"); };
+
+            Player.PlayerStateChanged += (sender, args) =>
+            {
+                switch(args.CurrentState)
+                {
+                    case PlayerState.Initialized:
+                        ExerciseTitle.Text = (index+1) +". " + Queue[index].Replace(".mp4", "");
+                        break;
+                    case PlayerState.Prepared:
+                        Player.Play(); 
+                        break;
+                    case PlayerState.Completed:
+                        if(index >= Queue.Length-1) index = -1;
+                        Player.Source = VideoSource.FromResource(Queue[++index]);
+                        break;
+                    case PlayerState.Error:
+                        DisplayAlert("Error Playing Video", "We're sorry but something is wrong with this video.", "OK");
+                        break;
+                }
+            };
+        }
+
+        private string[] Queue = { "LegRaise.mp4", "MountainClimer.mp4", "Plank.mp4", "Pushup.mp4", "Sideplank.mp4", "Situp.mp4" };
+        private int index;
+
+    }
 }
